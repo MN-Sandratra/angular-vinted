455460f6b697e4d9574e5b7e5421a21fc05caa0b
RA-MICHAEL <mikarasoloarison@gmail.com>
2021-08-27 06:09:41 +0300

    Achat detail

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatInputModule } from '@angular/material/input';
+
+import { MatDividerModule } from '@angular/material/divider';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { MatCardModule } from '@angular/material/card';
+
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+import {MatDatepickerModule} from '@angular/material/datepicker'; 
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatDividerModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatTooltipModule,
+  MatTabsModule,
+  MatGridListModule,
+  MatSnackBarModule,
+  MatDatepickerModule,
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MaterialComponents
+  ],
+  exports: [
+    MaterialComponents
+  ]
+})
+export class MaterialModule { }

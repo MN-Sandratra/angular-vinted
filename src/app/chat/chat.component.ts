4e6e27978cef2b3921a6231c65947cb1339e2aa9
MN-Sandratra <mn.sandratra@gmail.com>
2021-08-30 15:29:14 +0300

    update chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
+  public chat=[
+    {
+      message:`Saika isera entana azafady. Miady varota ve sa tode "fixe fixe" ? 😁`,
+      status:`receiver ms-auto`,
+    },
+    {
+      message:`Okalo zoky fa makafoky ny alina 🥵, rapitso ny sera`,
+      status:`sender me-auto`,
+    },
+    {
+      message:`Okay zoky be 😁`,
+      status:`receiver ms-auto`,
+    },
+  ]
+
   isEmojiPickerVisible: boolean = false;  
   message:string = "";
   
@@ -17,7 +32,11 @@ export class ChatComponent implements OnInit {
 
   onPost() {
     if(this.message !== "\n" && this.message !== "") {
-      
+      let data={
+        message:this.message,
+        status:'receiver ms-auto'
+      }
+      this.chat.push(data);
     }
     this.message = "";
   }
